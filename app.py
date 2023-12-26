5575ced81e99670066f7738f294b96bfb42f32dd
Mpsingh20 <154215977+Mpsingh20@users.noreply.github.com>
2023-12-26 10:32:12 +0000

    Added route

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "Hi man"
+  return render_template('home.html')
 
+@app.route("/about")
+def about_me():
+  return "This is about me"
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
